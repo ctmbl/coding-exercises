6fac1915e5b62655c7dbc9566b053996013b9cd4
ctmbl <mabileau.clement@gmail.com>
2022-12-14 17:36:02 -0500

    Add 002-max script

---
+++ exercises/002-max/max.py
@@ -0,0 +1,11 @@
+from solution.datas import L, answer 
+# define a function that takes L as argument and return the bigger element
+# !! it MUST be called `max_list` !!
+
+# code goes here
+
+### ------------------------------------------------- 
+if max_list(L) == answer:
+    print("Well done you solved this!")
+else:
+    print("Try again you're probably pretty close! (if in need you can check the hint)")
