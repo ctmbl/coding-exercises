79bedf53ec97607255f30a13793ffcfd110ba818
ctmbl <mabileau.clement@gmail.com>
2022-12-14 17:25:30 -0500

    Add 001-average script

---
+++ exercises/001-average/average.py
@@ -0,0 +1,11 @@
+from solution/data import L, answer 
+# define a function that takes L as argument and return its mean
+# !! it MUST be called `mean` !!
+
+# here
+
+### ------------------------------------------------- 
+if mean(L) == answer:
+    print("Well done you solved this!")
+else:
+    print("Try again you're probably pretty close!")
